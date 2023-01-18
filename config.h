257041102745f3dbf3a67ab47e6735c3481262d4
luo029 <luo029@outlook.com>
2023-01-19 00:38:51 +0800

    10.修改状态栏颜色

--- config.h
+++ config.h
@@ -8,10 +8,10 @@ static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Fira Code Nerd Mono:size=29" };
 static const char dmenufont[]       = "Fira Code Nerd Mono=29";
 static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
+static const char col_gray2[]       = "#999999";
 static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray4[]       = "#ffffff";
+static const char col_cyan[]        = "#37374f";
 static const unsigned int baralpha = 0xd0;
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
